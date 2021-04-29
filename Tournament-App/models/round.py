16d94ab68a2f7c772356a8739093b8a6610a553f
NicolasPython <demangeatn@gmail.com>
2021-04-29 19:56:32 +0200

    ajout round.py et tournament.py

---
+++ Tournament-App/models/round.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import os
+import datetime
+
+class Round:
+    '''Class management round'''
+
+    round_number = 1
+
+    def __init__(self):
+        self.name = f'Round {type(self).round_number}'
+        self.start_date = datetime.datetime.now().strftime("Date: %d/%m/%Y \nHeure: %H:%M:%S")
+        self.end_date = None
+        self.matchs = []
+        type(self).round_number += 1
+    
+    @classmethod
+    def reset_round_number(cls):
+        cls.round_number = 1
+    
+    def set_end_date(self):
+        '''Set the date and time when round is finish'''
+        self.end_date = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
---
+++ Tournament-App/models/tournament.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import os
+import datetime
+
+class Tournament:
+    '''Class management tournament'''
+
+    def __init__(self, name, place, nb_rounds, time_control, description):
+        self.name = name
+        self.place = place
+        self.nb_rounds = nb_rounds
+        self.time_control = time_control
+        self.description = description
+        
+    @property
+    def date(self):
+        now = datetime.datetime.now()
+        return now.strftime("%d/%m/%Y")
+
+    @property
+    def get_rounds(self):
+        #appel vers la BDD pour afficher la liste des instances de round du tournoi
+        pass
+
+    @property
+    def get_players(self):
+        #appel vers la BDD pour afficher la iste des indices correspondant aux instances du joueur
+        pass
+
+if __name__ == '__main__':
+    tournoi = Tournament('test', 'ici', 4, 'bullet', 'un tournoi')
+    print(tournoi.date)
