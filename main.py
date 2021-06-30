cc24e0604ad9449f46b96ecf66312fdb571dff90
NicolasPython <demangeatn@gmail.com>
2021-06-30 14:55:44 +0200

    modification main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from TournamentApp.controllers.app import ApplicationController
+
+
+def main():
+    app = ApplicationController()
+    app.start()
+
+
+if __name__ == "__main__":
+    main()
